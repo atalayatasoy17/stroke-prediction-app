4e94cf868b0dda364d176600c8a98525d346fbcd
Atalay Atasoy <atalay.atasoy@ozu.edu.tr>
2026-05-16 20:48:54 +0300

    Add data ingestion module

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -1,13 +1,7 @@
 import pandas as pd
 
-DATA_URL = "https://raw.githubusercontent.com/atalayatasoy17/stroke-prediction-app/refs/heads/main/data/healthcare-dataset-stroke-data.csv"
+DATA_URL = "https://raw.githubusercontent.com/atalayatasoy17/stroke-prediction-app/main/data/healthcare-dataset-stroke-data.csv"
 
-def fetch_data():
-    print("Veri indiriliyor...")
+def load_data():
     df = pd.read_csv(DATA_URL)
-    print(f"Veri yüklendi. Satır: {df.shape[0]}, Sütun: {df.shape[1]}")
     return df
-
-if __name__ == "__main__":
-    df = fetch_data()
-    print(df.head())
