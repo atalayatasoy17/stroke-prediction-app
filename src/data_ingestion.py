5cb59ceea31bcc70681c225435508198643b8b67
Atalay Atasoy <atalay.atasoy@ozu.edu.tr>
2026-05-17 00:08:35 +0300

    Clean slate: remove old src, app, data folders

--- src/data_ingestion.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-DATA_URL = "https://raw.githubusercontent.com/atalayatasoy17/stroke-prediction-app/main/data/healthcare-dataset-stroke-data.csv"
-
-def load_data():
-    df = pd.read_csv(DATA_URL)
-    return df
